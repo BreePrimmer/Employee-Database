5da4242981542ef2fcfefe3f78720d25b643339a
breelovesfrogs <134086243+breelovesfrogs@users.noreply.github.com>
2023-09-08 21:36:06 -0500

    created the menu for the program and tested if functions would run based on user input.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
+const mysql2 = require('mysql2');
 
-inquirer.prompt ([
+// data for the inquirer prompt to show the db menu.
+const dbMenu = [
     {
         type: "list",
         name: "menu",
         message: "What would you like to do?",
-        choices: ['View All Employees', 'Add Employees', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All departments', 'Add Department']
+        choices: ['View All Employees',
+                  'Add Employees', 
+                  'Update Employee Role', 
+                  'View All Roles', 
+                  'Add Role', 
+                  'View All Departments', 
+                  'Add Department',
+                  'Quit']
     }
-]);
+]
+
+// function to view the employee table
+viewAllEmployees = () => {
+    console.log('You are now viewing the employees (:')
+}
+
+// function to add an employee to the table
+addEmployees = () => {
+    console.log('Would you like to add an employee?')
+}
+
+// function to start the menu and run the functions based on the response.
+const startMenu = () => {
+    inquirer.prompt(dbMenu).then((response) => {
+        console.log(response.menu)
+        if (response.menu == 'View All Employees') {
+            viewAllEmployees()
+        } else if (response.menu == 'Add Employees') {
+            addEmployees()
+        } else {
+            return
+        }
+
+})
+}
+
+startMenu()
 
